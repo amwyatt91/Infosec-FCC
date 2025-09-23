b33d68450f6512cb6751e11fb07790c9f285a9a0
Andi <155691606+amwyatt91@users.noreply.github.com>
2025-09-23 15:24:50 -0500

    hsts

--- myApp.js
+++ myApp.js
@@ -10,6 +10,10 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
+app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
+const timeInSeconds = 90 * 24 * 60 * 60;
+
+
 
 
 
